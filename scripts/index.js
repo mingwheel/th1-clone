8d0bc7ccd2368e0cea388c4b8695ae45141f8bab
Ming <dev.sohml@gmail.com>
2019-07-08 15:32:03 +0800

    Fix anchor smooth scrolling

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,11 @@
-//  throttle scroll
-function throttle(fn, wait) {
-  var time = Date.now();
-  return function() {
-    if (time + wait - Date.now() < 0) {
-      fn();
-      time = Date.now();
-    }
-  };
-}
 const navbar = document.querySelector(".navbar");
 const navlinks = document.querySelectorAll(".navbar-menu a");
+
+/* Nav menu smooth scrolling */
+const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  easing: "easeInOutQuart"
+});
 
 function fixNav() {
   if (window.scrollY > 100) {
@@ -21,24 +17,13 @@ function fixNav() {
 
 window.addEventListener("scroll", throttle(fixNav, 20));
 
-function scroll(elementY, duration) {
-  var startingY = window.pageYOffset;
-  var diff = elementY - startingY;
-  var start;
-
-  // Bootstrap our animation - it will get called right before next frame shall be rendered.
-  window.requestAnimationFrame(function step(timestamp) {
-    if (!start) start = timestamp;
-    // Elapsed milliseconds since start of scrolling.
-    var time = timestamp - start;
-    // Get percent of completion in range [0, 1].
-    var percent = Math.min(time / duration, 1);
-
-    window.scrollTo(0, startingY + diff * percent);
-
-    // Proceed with animation as long as we wanted it to.
-    if (time < duration) {
-      window.requestAnimationFrame(step);
+//  throttle scroll
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function() {
+    if (time + wait - Date.now() < 0) {
+      fn();
+      time = Date.now();
     }
-  });
+  };
 }
